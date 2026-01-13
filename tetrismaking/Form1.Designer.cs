dd21cfd5d939217faab9c2ae009f6de866e4a8b7
kiransimmons10-art <kiransimmons10@gmail.com>
2026-01-13 11:19:34 -0500

    added a game end function, squares to check if overfull, and made it so movement dont work when you enter the grid

--- tetrismaking/Form1.Designer.cs
+++ tetrismaking/Form1.Designer.cs
@@ -66,7 +66,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(451, 450);
+            this.ClientSize = new System.Drawing.Size(443, 562);
             this.Controls.Add(this.scorelabel);
             this.DoubleBuffered = true;
             this.Name = "Form1";
--- tetrismaking/Form1.cs
+++ tetrismaking/Form1.cs
@@ -19,27 +19,38 @@ namespace tetrismaking
             Pen greyPen = new Pen(Color.Gray, 3);
        
         //invisible swuares n stuff
-        Rectangle a1 = new Rectangle(60, 100, 70, 70);
-        Rectangle a2 = new Rectangle(135, 100, 70, 70);
-        Rectangle a3 = new Rectangle(210, 100, 70, 70);
-        Rectangle a4 = new Rectangle(285, 100, 70, 70); 
-        Rectangle b1 = new Rectangle(60, 175, 70, 70);
-        Rectangle b2 = new Rectangle(135, 175, 70, 70);
-        Rectangle b3 = new Rectangle(210, 175, 70, 70);
-        Rectangle b4 = new Rectangle(285, 175, 70, 70);
-        Rectangle c1 = new Rectangle(60, 250, 70, 70);
-        Rectangle c2 = new Rectangle(135, 250, 70, 70);
-        Rectangle c3 = new Rectangle(210, 250, 70, 70);
-        Rectangle c4 = new Rectangle(285, 250, 70, 70);
-        Rectangle d1 = new Rectangle(60, 325, 70, 70);
-        Rectangle d2 = new Rectangle(135, 325, 70, 70);
-        Rectangle d3 = new Rectangle(210, 325, 70, 70);
-        Rectangle d4 = new Rectangle(285, 325, 70, 70);
+        Rectangle a1 = new Rectangle(60, 250, 70, 70);
+        Rectangle a2 = new Rectangle(135, 250, 70, 70);
+        Rectangle a3 = new Rectangle(210, 250, 70, 70);
+        Rectangle a4 = new Rectangle(285, 250, 70, 70); 
+        Rectangle b1 = new Rectangle(60, 325, 70, 70);
+        Rectangle b2 = new Rectangle(135, 325, 70, 70);
+        Rectangle b3 = new Rectangle(210, 325, 70, 70);
+        Rectangle b4 = new Rectangle(285, 325, 70, 70);
+        Rectangle c1 = new Rectangle(60, 400, 70, 70);
+        Rectangle c2 = new Rectangle(135, 400, 70, 70);
+        Rectangle c3 = new Rectangle(210, 400, 70, 70);
+        Rectangle c4 = new Rectangle(285, 400, 70, 70);
+        Rectangle d1 = new Rectangle(60, 475, 70, 70);
+        Rectangle d2 = new Rectangle(135, 475, 70, 70);
+        Rectangle d3 = new Rectangle(210, 475, 70, 70);
+        Rectangle d4 = new Rectangle(285, 475, 70, 70);
 
         //live square 
         Rectangle live = new Rectangle(60, 25, 70, 70);
 
-       
+        //top squares that sense if the game ends
+        Rectangle e1 = new Rectangle(60, 175, 70, 70);
+        Rectangle e2 = new Rectangle(135, 175, 70, 70);
+        Rectangle e3 = new Rectangle(210, 175, 70, 70);
+        Rectangle e4 = new Rectangle(285, 175, 70, 70);
+        //and their respective ints 
+        int E1 = 0;
+        int E2 = 0;
+        int E3 = 0;
+        int E4 = 0;
+
+
         //if occupied ints
         int A1 = 0;
         int A2 = 0;
@@ -79,6 +90,8 @@ namespace tetrismaking
         //int to determine the shape of the block
         int shape = 1;
 
+        //if you have or have not lost 
+        Boolean lost = false;
         public Form1()
         {
            
@@ -90,8 +103,19 @@ namespace tetrismaking
             
             Invalidate();
             //checking if each grid space intersects with the player and then upping their int
+            //if a 1 by 1 square so only checks square under it 
             if (shape == 1)
             {
+                if (e1.IntersectsWith(live) && A1 == 1)
+                    gameend();//gameend triggers if the squares above are triggered and you lose 
+                if (e2.IntersectsWith(live) && A2 == 1)
+                    gameend();
+                if (e3.IntersectsWith(live) && A3 == 1)
+                    gameend();
+                if (e4.IntersectsWith(live) && A4 == 1)
+                    gameend();
+
+
                 if (a1.IntersectsWith(live) && B1 == 1)
                 { A1 = 1; returnlive(); }
 
@@ -141,18 +165,18 @@ namespace tetrismaking
                 { D4 = 1; returnlive(); }
             }
             if (shape == 2)
-            {
+            {//tall shape 2x1, senses the bottom square and then manually fils in the one above 
                 if (a1.IntersectsWith(live) && B1 == 1)
-                { A1 = 1; returnlive(); }
+                { A1 = 1; gameend(); }
 
                 if (a2.IntersectsWith(live) && B2 == 1)
-                { A2 = 1; returnlive(); }
+                { A2 = 1;gameend(); }
 
                 if (a3.IntersectsWith(live) && B3 == 1)
-                { A3 = 1; returnlive(); }
+                { A3 = 1; gameend(); }
 
                 if (a4.IntersectsWith(live) && B4 == 1)
-                { A4 = 1; returnlive(); }
+                { A4 = 1; gameend(); }
 
                 if (b1.IntersectsWith(live) && C1 == 1)
                 { B1 = 1; A1 = 1; returnlive(); }
@@ -192,7 +216,18 @@ namespace tetrismaking
 
             }
             if (shape == 3)
-            {
+            {//wide square, only has three lateral positions so only three per level
+                if (e1.IntersectsWith(live) && A1 == 1)
+                { A1 = 1; A2 = 1; gameend(); }
+
+                if (e2.IntersectsWith(live) && A2 == 1)
+                { A2 = 1; A3 = 1; gameend(); }
+
+                if (e3.IntersectsWith(live) && A3 == 1)
+                { A3 = 1; A4 = 1; gameend(); }
+
+
+
                 if (a1.IntersectsWith(live) && B1 == 1)
                 { A1 = 1; A2 = 1; returnlive(); }
 
@@ -406,78 +441,81 @@ namespace tetrismaking
 
 
         private void Form1_Paint(object sender, PaintEventArgs e)
-        {
+        {//initializing graphics 
             Graphics g = e.Graphics;
-          
-            //generating backround grid 
-            if (A1 == 0)
-            g.FillRectangle(blackBrush, a1);
-            else 
-            g.FillRectangle(blueBrush, a1);
-            if (A2 == 0)
-                g.FillRectangle(blackBrush, a2);
-            else
-                g.FillRectangle(blueBrush, a2);
-            if (A3 == 0)
-                g.FillRectangle(blackBrush, a3);
-            else
-                g.FillRectangle(blueBrush, a3);
-            if (A4 == 0)
-                g.FillRectangle(blackBrush, a4);
-            else 
-                g.FillRectangle(blueBrush,a4);
-            if (B1 == 0)
-                g.FillRectangle(blackBrush, b1);
-            else
-                g.FillRectangle(blueBrush, b1);
-            if (B2 == 0)
-                g.FillRectangle(blackBrush, b2);
-            else
-                g.FillRectangle(blueBrush, b2);
-            if (B3 == 0)
-                g.FillRectangle(blackBrush, b3);
-            else
-                g.FillRectangle(blueBrush, b3);
-            if (B4 == 0)
-                g.FillRectangle(blackBrush, b4);
-            else
-                g.FillRectangle(blueBrush, b4);
-            if (C1 == 0)
-                g.FillRectangle(blackBrush, c1);
-            else
-                g.FillRectangle(blueBrush, c1);
-            if (C2 == 0)
-                g.FillRectangle(blackBrush, c2);
-            else
-                g.FillRectangle(blueBrush, c2);
-            if (C3 == 0)
-                g.FillRectangle(blackBrush, c3);
-            else
-                g.FillRectangle(blueBrush, c3);
-            if (C4 == 0)
-                g.FillRectangle(blackBrush, c4);
-            else
-                g.FillRectangle(blueBrush, c4);
-            if (D1 == 0)
-                g.FillRectangle(blackBrush, d1);
-            else
-                g.FillRectangle(blueBrush, d1);
-            if (D2 == 0)
-                g.FillRectangle(blackBrush, d2);
-            else
-                g.FillRectangle(blueBrush, d2);
-            if (D3 == 0)
-                g.FillRectangle(blackBrush, d3);
-            else
-                g.FillRectangle(blueBrush, d3);
-            if (D4 == 0)
-                g.FillRectangle(blackBrush, d4);
-            else
-                g.FillRectangle(blueBrush, d4);
+          //what shows if you have not lost 
+            if (lost == false)
+            {
+                //generating backround grid 
+                if (A1 == 0)
+                    g.FillRectangle(blackBrush, a1);
+                else
+                    g.FillRectangle(blueBrush, a1);
+                if (A2 == 0)
+                    g.FillRectangle(blackBrush, a2);
+                else
+                    g.FillRectangle(blueBrush, a2);
+                if (A3 == 0)
+                    g.FillRectangle(blackBrush, a3);
+                else
+                    g.FillRectangle(blueBrush, a3);
+                if (A4 == 0)
+                    g.FillRectangle(blackBrush, a4);
+                else
+                    g.FillRectangle(blueBrush, a4);
+                if (B1 == 0)
+                    g.FillRectangle(blackBrush, b1);
+                else
+                    g.FillRectangle(blueBrush, b1);
+                if (B2 == 0)
+                    g.FillRectangle(blackBrush, b2);
+                else
+                    g.FillRectangle(blueBrush, b2);
+                if (B3 == 0)
+                    g.FillRectangle(blackBrush, b3);
+                else
+                    g.FillRectangle(blueBrush, b3);
+                if (B4 == 0)
+                    g.FillRectangle(blackBrush, b4);
+                else
+                    g.FillRectangle(blueBrush, b4);
+                if (C1 == 0)
+                    g.FillRectangle(blackBrush, c1);
+                else
+                    g.FillRectangle(blueBrush, c1);
+                if (C2 == 0)
+                    g.FillRectangle(blackBrush, c2);
+                else
+                    g.FillRectangle(blueBrush, c2);
+                if (C3 == 0)
+                    g.FillRectangle(blackBrush, c3);
+                else
+                    g.FillRectangle(blueBrush, c3);
+                if (C4 == 0)
+                    g.FillRectangle(blackBrush, c4);
+                else
+                    g.FillRectangle(blueBrush, c4);
+                if (D1 == 0)
+                    g.FillRectangle(blackBrush, d1);
+                else
+                    g.FillRectangle(blueBrush, d1);
+                if (D2 == 0)
+                    g.FillRectangle(blackBrush, d2);
+                else
+                    g.FillRectangle(blueBrush, d2);
+                if (D3 == 0)
+                    g.FillRectangle(blackBrush, d3);
+                else
+                    g.FillRectangle(blueBrush, d3);
+                if (D4 == 0)
+                    g.FillRectangle(blackBrush, d4);
+                else
+                    g.FillRectangle(blueBrush, d4);
 
 
-            //generating live square 
-            g.FillRectangle(greyBrush, live);
+                //generating live square 
+                g.FillRectangle(greyBrush, live);
+            }
 
           
 
@@ -492,67 +530,81 @@ namespace tetrismaking
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
-        {//chaking when keys are pressed down and activating their boolean
+        {
+           //movement for each shape
+           //1x1 square so has many degrees of freedom 
             if (shape == 1)
-            {
-                switch (e.KeyCode)
+            {//only works above the grid, once within can no longer move 
+                if (live.Y < 250)
                 {
-                    case Keys.A:
-                        if (live.X > 60)
-                            live.X -= 75;
-                        break;
-                    case Keys.D:
-                        if (live.X < 275)
-                            live.X += 75;
-                        break;
-                    case Keys.S:
-                        live.Y += 75;
-                        break;
+                    switch (e.KeyCode)
+                    {
+                        case Keys.A:
+                            if (live.X > 60)
+                                live.X -= 75;
+                            break;
+                        case Keys.D:
+                            if (live.X < 275)
+                                live.X += 75;
+                            break;
+                        case Keys.S:
+                            live.Y += 75;
+                            break;
+                    }
                 }
-            }
+            }//tall square, mostly simmilar to 1x1
             if (shape == 2)
             {
-                   
-                
-                switch (e.KeyCode)
+
+                if (live.Y < 250)
                 {
-                    case Keys.A:
-                        if (live.X > 60)
-                            live.X -= 75;
-                        break;
-                    case Keys.D:
-                        if (live.X < 275)
-                            live.X += 75;
-                        break;
-                    case Keys.S:
-                        live.Y += 75;
-                        break;
-                   
+                    switch (e.KeyCode)
+                    {
+                        case Keys.A:
+                            if (live.X > 60)
+                                live.X -= 75;
+                            break;
+                        case Keys.D:
+                            if (live.X < 275)
+                                live.X += 75;
+                            break;
+                        case Keys.S:
+                            live.Y += 75;
+                            break;
+
+                    }
                 }
             }
+            //wide shape, cant move too far to the right so it doesnt clip out of bounds 
             if (shape == 3)
             {
                 if (live.X > 210)
                     live.X = 210;
-
-                switch (e.KeyCode)
+                if (live.Y < 250)
                 {
-                    case Keys.A:
-                        if (live.X > 60)
-                            live.X -= 75;
-                        break;
-                    case Keys.D:
-                        if (live.X < 200)
-                            live.X += 75;
-                        break;
-                    case Keys.S:
-                        live.Y += 75;
-                        break;
+                    switch (e.KeyCode)
+                    {
+                        case Keys.A:
+                            if (live.X > 60)
+                                live.X -= 75;
+                            break;
+                        case Keys.D:
+                            if (live.X < 200)
+                                live.X += 75;
+                            break;
+                        case Keys.S:
+                            live.Y += 75;
+                            break;
 
+                    }
                 }
             }
             }
-
+        private void gameend() 
+        {
+            lost = true;
+        
+        }
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
             
